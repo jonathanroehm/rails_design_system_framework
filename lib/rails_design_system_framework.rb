772ec81a8620a03dd6d4d2a9cd990275ed7b2984
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-06-12 13:11:08 -0400

    Initial commit

---
+++ lib/rails_design_system_framework.rb
@@ -0,0 +1,6 @@
+require "rails_design_system_framework/version"
+
+module RailsDesignSystemFramework
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rails_design_system_framework/version.rb
@@ -0,0 +1,3 @@
+module RailsDesignSystemFramework
+  VERSION = "0.1.0"
+end
