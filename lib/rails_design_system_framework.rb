4e57f6406ff23dfbcd76d800ac08e08e2d694d55
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-06-12 13:31:32 -0400

    Sets up an initial configuration.

--- lib/rails_design_system_framework.rb
+++ lib/rails_design_system_framework.rb
@@ -1,6 +1,18 @@
 require "rails_design_system_framework/version"
+require "rails_design_system_framework/configuration"
 
 module RailsDesignSystemFramework
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    attr_writer :configuration
+
+    def configuration
+      @configuration ||= RailsDesignSystemFramework::Configuration.new
+    end
+
+    def configure
+      yield configuration
+    end
+  end
 end
---
+++ lib/rails_design_system_framework/configuration.rb
@@ -0,0 +1,7 @@
+module RailsDesignSystemFramework
+  class Configuration
+    def design_system_name
+      'default'
+    end
+  end
+end
